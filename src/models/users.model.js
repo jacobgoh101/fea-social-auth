726014724705d55df35ddc1d95a8ac73ed4325ea
Jacob Goh <jacobgoh101@gmail.com>
2017-08-20 17:19:42 +0800

    modify user model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,10 +5,20 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
-  
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now
+    }
   });
 
   return mongooseClient.model('users', users);

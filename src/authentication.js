029551a08663af808161eca91f686249c8a7175f
Jacob Goh <jacobgoh101@gmail.com>
2017-08-21 22:48:14 +0800

    bk

--- src/authentication.js
+++ src/authentication.js
@@ -1,7 +1,6 @@
 const authentication = require('feathers-authentication');
 const jwt = require('feathers-authentication-jwt');
-
-
+const CustomStrategy = require('passport-custom');
 
 module.exports = function () {
   const app = this;
@@ -11,17 +10,33 @@ module.exports = function () {
   app.configure(authentication(config));
   app.configure(jwt());
 
-  // The `authentication` service is used to create a JWT.
-  // The before `create` hook registers strategies that can be used
-  // to create a new valid JWT (e.g. local or oauth2)
-  app.service('authentication').hooks({
-    before: {
-      create: [
-        authentication.hooks.authenticate(config.strategies)
-      ],
-      remove: [
-        authentication.hooks.authenticate('jwt')
-      ]
-    }
-  });
+  // custom passport strategy for client side social login
+  app
+    .passport
+    .use('client-social-token', new CustomStrategy(function (req, callback) {
+      // Do your custom user finding logic here, or set to false based on req object
+      // callback(null, user);
+      console.log(req.body);
+      callback(null, false);
+    }));
+
+  // The `authentication` service is used to create a JWT. The before `create`
+  // hook registers strategies that can be used to create a new valid JWT (e.g.
+  // local or oauth2)
+  app
+    .service('authentication')
+    .hooks({
+      before: {
+        create: [
+          authentication
+            .hooks
+            .authenticate(config.strategies)
+        ],
+        remove: [
+          authentication
+            .hooks
+            .authenticate('jwt')
+        ]
+      }
+    });
 };
